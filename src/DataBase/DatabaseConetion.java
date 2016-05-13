a37f93b14a72384d6bf65389e92de07e4eb84b37
Assasin <dorineltuta2006@yahoo.com>
2016-05-13 18:39:24 +0300

    Database Setters Added

--- src/DataBase/DatabaseConetion.java
+++ src/DataBase/DatabaseConetion.java
@@ -11,6 +11,35 @@ public class DatabaseConetion {
 	
 	protected Connection con;
 	protected java.sql.Statement stm;
+	
+	//***Login***\\
+	private String user;
+	private String password;
+	private String mod;
+	
+	//***Person***\\
+	private String cnp;
+	private String name;
+	private String pname;
+	private String addres;
+	private String tara;
+	
+	//***Medic***\\
+	private String parafa;
+	private String ambulatoriu;
+	
+	//***Admin***\\
+	private String drepturi;
+	
+	
+	//***Reteta***\\
+	private String id;
+	private String date;
+	private String iagnose;
+	private String observatii;
+	private ArrayList<String> medicatie;      //p[0]-medicatie, p[1]-cantitate, p[2]- ....
+	
+	
 	//protected ResultSet ru;
 	
 	
@@ -46,33 +75,7 @@ public class DatabaseConetion {
 	}
 	
 	
-	
-//	public String Register(ArrayList<String> data){
-//		
-//		String user 	= data.get(0);
-//		String password = data.get(1);
-//		String Mod 		= data.get(2);
-//		
-//		try {
-//			
-//			String query = "INSERT INTO 'logindata'.'data' ('ID', 'Password', 'Mod') VALUES ('"+user+"', '"+password+"', '"+Mod+"');";
-//
-//			stm = con.createStatement();
-//			ResultSet rs = stm.executeQuery(query);
-//			
-//			System.out.println("am reusit sa introduc urmatoarele date: " + user + " " + password + " " + Mod );
-//		
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//		}
-//		return null;
-//	}
-	
-	
-	
-	
-	
-	public void Register(ArrayList<String> data){
+	public void Register(){
 		
 		try
 	    {
@@ -89,21 +92,14 @@ public class DatabaseConetion {
 	      // the mysql insert statement
 	      String query = "insert into data values (?, ?, ?)";
 	      
-	      
-	      	String user 	= data.get(0);
-			String password = data.get(1);
-			String Mod 		= data.get(2);
-			
-	 
 	      // create the mysql insert preparedstatement
 	      java.sql.PreparedStatement preparedStmt = conn.prepareStatement(query);
 	      preparedStmt.setString (1, user);
 	      preparedStmt.setString (2, password);
-	      preparedStmt.setString (3, Mod);
+	      preparedStmt.setString (3, mod);
 	      
 	      // execute the preparedstatement
 	      preparedStmt.executeUpdate();
-	       
 	      
 	      conn.close();
 	    }
@@ -112,5 +108,95 @@ public class DatabaseConetion {
 	      System.err.println("Got an exception!");
 	      System.err.println(e.getMessage());
 	    }
+	}
+
+
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+
+
+	public void setStm(java.sql.Statement stm) {
+		this.stm = stm;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public void setMod(String mod) {
+		this.mod = mod;
+	}
+
+
+	public void setCnp(String cnp) {
+		this.cnp = cnp;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+
+	public void setAddres(String addres) {
+		this.addres = addres;
+	}
+
+
+	public void setTara(String tara) {
+		this.tara = tara;
+	}
+
+
+	public void setParafa(String parafa) {
+		this.parafa = parafa;
+	}
+
+
+	public void setAmbulatoriu(String ambulatoriu) {
+		this.ambulatoriu = ambulatoriu;
+	}
+
+
+	public void setDrepturi(String drepturi) {
+		this.drepturi = drepturi;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+
+	public void setIagnose(String iagnose) {
+		this.iagnose = iagnose;
+	}
+
+
+	public void setObservatii(String observatii) {
+		this.observatii = observatii;
+	}
+
+
+	public void setMedicatie(ArrayList<String> medicatie) {
+		this.medicatie = medicatie;
+	}
+
+
+	public void setUser(String user) {
+		this.user = user;
 	}	
 }
